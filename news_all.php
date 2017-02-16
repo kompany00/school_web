20f35faf8b44fe2a68acf7a6571fda7fe1f035a1
kompany00 <1211351640@qq.com>
2017-02-16 14:56:35 +0800

    Add files via upload

---
+++ news_all.php
@@ -0,0 +1,33 @@
+<?php
+include "conn/conn.php";
+include "header.php";
+?>
+
+
+<?php
+$query = "select title, id from sjfh_news";
+$res = mysqli_query($dbc, $query);
+$ret = mysqli_fetch_all($res, MYSQLI_ASSOC);
+$len = count($ret);
+
+?>
+
+
+<div>
+<?php
+while($len)
+{
+?>
+
+    <a href="news.php?id=<?php echo "{$ret[2-$len]['id']}" ?>"><?php echo "{$ret[2-$len]['title']}" ?></a>
+
+
+<?php
+    $len -= 1;
+}
+?>
+
+</div>
+<?php
+include"footer.php";
+?>
